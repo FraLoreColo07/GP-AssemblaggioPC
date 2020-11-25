4122dfd51135909925136e3884c62f30a41c117f
FraLoreColo07 <57748531+FraLoreColo07@users.noreply.github.com>
2020-11-25 15:06:24 +0100

    aggiornamento file

---
+++ file/Denki-system/_script/script2.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    $("#button1-cpu").click(function () {
+
+        document.getElementById("prova").innerHTML = "Fatto";
+    });
+    $("#button1-mo").click(function () {
+
+        document.getElementById("prova").innerHTML = "Fatto1";
+    });
+    $("#button1-ram").click(function () {
+
+        document.getElementById("prova").innerHTML = "Fatto2";
+    });
+    $("#button1-gpu").click(function () {
+
+        document.getElementById("prova").innerHTML = "Fatto3";
+    });
+    $("#button1-ssd").click(function () {
+
+        document.getElementById("prova").innerHTML = "Fatto4";
+    });
+    $("#button1-case").click(function () {
+
+        document.getElementById("prova").innerHTML = "Fatto5";
+    });
+});
+
+
